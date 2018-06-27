d59d8642b6575bdd9d6097a1c1ef57a1d6f4bca2
Chris Del <drumgod101@gmail.com>
2018-06-27 03:56:01 +0000

    cnahge to ameil, add email into db

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 import os
 import datetime
+import re
+
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
@@ -206,21 +208,21 @@ def login():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+        # Ensure email was submitted
+        if not request.form.get("email"):
+            return apology("must provide email", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
+        # Query database for email
+        rows = db.execute("SELECT * FROM users WHERE email = :email",
+                          email=request.form.get("email"))
 
-        # Ensure username exists and password is correct
+        # Ensure email exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid email and/or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -271,24 +273,28 @@ def quote():
 def register():
     # user GET, then just render template
     if request.method == "POST":
-        if not request.form.get("username"):
-            return apology("must provide username", 400)
+        if not request.form.get("email"):
+            return apology("must provide email", 400)
         if not request.form.get("password"):
             return apology("must provide password", 400)
         elif request.form.get("password") != request.form.get("confirmation"):
             return apology("passwords must match", 400)
 
+        email = request.form.get("email")
         # # generate password hash
-        username = request.form.get("username")
         hash = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
-        username_query = db.execute("SELECT username FROM users WHERE username=(:username)", username=username)
-        if username_query != []:
-            return apology("That username already exists. Choose another.", 400)
+        # check that is email syntax
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            flash("Not valid email syntax")
+            return render_template("register.html")
+        email_query = db.execute("SELECT email FROM users WHERE email=(:email)", email=email)
+        if email_query != []:
+            return apology("That email already exists. Choose another.", 400)
 
-        db.execute("INSERT INTO users (username,hash) VALUES (:username, :hash)", username=username, hash=hash)
-        user_id = db.execute("SELECT id FROM users WHERE username=(:username)", username=username)
+        db.execute("INSERT INTO users (email,hash) VALUES (:email, :hash)", email=email, hash=hash)
+        user_id = db.execute("SELECT id FROM users WHERE email=(:email)", email=email)
         session["user_id"] = user_id
-
+        flash("You are registed.")
         return render_template("index.html")
 
     elif request.method == "GET":
